825b9348ec7d8b2f580d825b87a2945c829381d8
Angly-eng <676836584@qq.com>
2022-12-26 20:21:42 +0800

    第一次提交

---
+++ init_key/api_key.py
@@ -0,0 +1,14 @@
+# -*- coding = utf-8 -*-
+from random import choice
+
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy as By
+from appium.webdriver.common.mobileby import AppiumBy
+
+# MobileElement el1 = (MobileElement) driver.findElementById("com.zhao.myreader:id/iv_search");
+# el1.click();
+# MobileElement el2 = (MobileElement) driver.findElementById("com.zhao.myreader:id/et_search_key");
+# el2.sendKeys("九重华锦");
+# MobileElement el3 = (MobileElement) driver.findElementById("com.zhao.myreader:id/tv_search_conform");
+# el3.click();
+
